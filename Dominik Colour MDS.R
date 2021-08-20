9668107a6e3af3010c6b8a52a94c13298d5e59ed
ArielZJ <ariel.zeleznikow-johnston@monash.edu>
2021-08-20 11:50:07 +1000

    Improved a bit

--- Dominik Colour MDS.R
+++ Dominik Colour MDS.R
@@ -14,31 +14,49 @@ rgb2hex <- function(triplet){
 
 colors <- rgb2hex(as.character(data.means$Row.RGB))
 data.means$Row.RGB <- colors # start by looking at mean responses
+data.medians$Row.RGB <- colors # start by looking at mean responses
+data.modes$Row.RGB <- colors # start by looking at mean responses
 
 # label the columns appropriately
-col.RGB <- rev(data.means$Row.RGB)
-colnames(data.means) <- col.RGB
-rownames(data.means) <- data.means$Row.RGB
-data.means <- subset(data.means, select = -c(94))
+relabel <- function(data){
+  col.RGB <- rev(data$Row.RGB)
+  colnames(data) <- col.RGB
+  rownames(data) <- data$Row.RGB
+  data <- subset(data, select = -c(94))
+  return(data)
+}
 
-# convert dataframe to distance matrix for MDS
-data.dist <- dist(data.means,diag=TRUE)
+# relabel the data frames
+data.medians <- relabel(data.medians)
+data.modes <- relabel(data.modes)
+data.means <- relabel(data.means)
+
+# plot stuff
+plot.data <- function(data){
+  # convert dataframe to distance matrix for MDS
+  data.dist <- dist(data,diag=TRUE)
+  
+  
+  # peform metric MDS, set dimensionality to 3
+  #Calculate MDS
+  data.mds <- cmdscale(data.dist, k=3)
+  
+  #Create x,y refs
+  data.x <- data.mds[,1]
+  data.y <- data.mds[,2]
+  data.z <- data.mds[,3]
+  
+  # visualise the data
+  plot <- plot3d(data.x,data.y,data.z, col = colors, size = 10)
+  
+  # 3D animated version
+  #play3d(spin3d(axis=c(1,1,0), rpm=3), duration=30)
+  
+  # save a screenshot
+  rgl.snapshot('3dplot.png', fmt = 'png')
+}
+
+# Do it
+plot.data(data.modes)
 
 
-# peform metric MDS, set dimensionality to 3
-#Calculate MDS
-data.mds <- cmdscale(data.dist, k=3)
-
-#Create x,y refs
-data.x <- data.mds[,1]
-data.y <- data.mds[,2]
-data.z <- data.mds[,3]
-
-# visualise the data
-plot <- plot3d(data.x,data.y,data.z, col = colors, size = 10)
-
-# 3D animated version
-#play3d(spin3d(axis=c(1,1,0), rpm=3), duration=30)
-
-# save a screenshot
-rgl.snapshot('3dplot.png', fmt = 'png')
